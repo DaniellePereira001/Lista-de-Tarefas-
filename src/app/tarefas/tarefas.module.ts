c9036ba61a399d9c307c7c4ffad5c3e494b9a0f1
DaniellePereira001 <danipereiradp@hotmail.com>
2022-11-10 18:13:57 -0300

    lista de tarefas

---
+++ src/app/tarefas/tarefas.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TarefaService } from './shared';
+import { ListarTarefaComponent } from './listar';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CadastrarTarefaComponent } from './cadastrar';
+import { EditarTarefaComponent } from './editar';
+import { TarefaConcluidaDirective } from './shared/tarefa-concluida.directive';
+
+
+
+@NgModule({
+  declarations: [
+    ListarTarefaComponent,
+    CadastrarTarefaComponent,
+    EditarTarefaComponent,
+    TarefaConcluidaDirective
+  ],
+
+  providers: [
+    TarefaService
+
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    FormsModule
+  ]
+})
+export class TarefasModule { }
